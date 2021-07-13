3319abaac6287dfac55b2a9f0f585951070720c1
cotamor <cotamor@gmail.com>
2021-07-13 18:42:59 +0900

    refactor elements on script.js

--- project-tracking-intro/script.js
+++ project-tracking-intro/script.js
@@ -1,16 +1,7 @@
-const menu = document.querySelector(".mobile-menu");
-const burger = document.querySelector(".burger");
-const close = document.querySelector(".close");
-const modal = document.querySelector(".modal");
+const hamburger = document.getElementById("hamburger");
+const { body } = document;
 
-menu.addEventListener("click", () => {
-  if (!menu.classList.contains("show")) {
-    burger.classList.toggle("show");
-    close.classList.toggle("show");
-    modal.classList.toggle("show");
-  } else {
-    burger.classList.toggle("show");
-    close.classList.toggle("show");
-    modal.classList.toggle("show");
-  }
+hamburger.addEventListener("click", () => {
+  // alert("hamburger! click");
+  body.classList.toggle("nav-show");
 });
